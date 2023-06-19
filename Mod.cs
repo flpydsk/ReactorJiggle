c3ee707dce7921590bfd60fd70b781d4525ace6f
FloppyDisk <FloppyDisk@no.no>
2023-06-19 19:43:29 +1200

    Update licence and copyright

--- Mod.cs
+++ Mod.cs
@@ -1,17 +1,33 @@
-﻿//Copyright 2023 (c) Floppydisk
-//GPL 3.0-only
+﻿/*
+    Reactor Jiggle With this mod we can increase the action in the engineering bay by having the core fly around in the reactor proportional to the temperature.
+    Copyright (C) 2023 FloppyDisk 18107
+    https://github.com/flpydsk/ReactorJiggle.git
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 using PulsarModLoader;
 
 namespace ReactorJiggle
 {
     public class Mod : PulsarMod
     {
-        public override string Version => "0.1";
+        public override string License => "GNU GPL-3.0";
+        public override string Version => "0.1.1";
         public override string Author => "FloppyDisk&18107";
         public override string Name => "ReactorJiggle";
         public override string HarmonyIdentifier() => $"{Author}.{Name}";
-        public override string ShortDescription => "Jiggles the reactor core";
-        public override string LongDescription => "Licence: GPL 3.0-only";
+        public override string ShortDescription => "Jiggles the reactor core with temp";
 
     }
 }
